3fdc4f7667593482e42b1f575885be49094708eb
Tri Nguyen <nductri44@gmail.com>
2023-04-10 11:48:26 +0700

    internal RTC

--- rtc32/main/rtc32.c
+++ rtc32/main/rtc32.c
@@ -1,19 +1,69 @@
-#include <stdio.h>
-#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_system.h"
+#include "driver/gpio.h"
+#include "driver/rtc_io.h"
+#include "esp_sleep.h"
+#include "spi_flash_mmap.h"
 #include "esp_timer.h"
-#include "rtc.h"
+#include "esp_err.h"
+#include "esp32/rom/rtc.h"
+#include "esp32/rom/crc.h"
+#include "esp32/rtc.h"
+#include "sys/time.h"
+#include <time.h>
 
-void app_main()
+#define TAG "RTC"
+
+void read_rtc(void *pvParameter)
 {
-    // initialize the RTC
-    rtc_gpio_init(4); 
+    while (1) {
+        time_t now;
+        struct tm timeinfo;
+        time(&now);
+        localtime_r(&now, &timeinfo);
+        printf("Current time: %04d-%02d-%02d %02d:%02d:%02d\n",
+               timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday,
+               timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
 
-    // set the time to the current time
-    int64_t current_time_us = esp_timer_get_time();
-    rtc_clk_set(current_time_us);
+void app_main(void)
+{
+    esp_err_t ret;
+    struct tm timeinfo;
+    time_t now;
 
-    // get the current time from the RTC
-    int64_t rtc_time_us = rtc_clk_get();
-    printf("RTC time: %lld microseconds\n", rtc_time_us);
+    // Set default time to January 1st, 2023 at 00:00:00
+    struct tm default_time = {
+        .tm_year = 2023 - 1900,
+        .tm_mon = 0, // January
+        .tm_mday = 1,
+        .tm_hour = 0,
+        .tm_min = 0,
+        .tm_sec = 0
+    };
+    time_t default_time_secs = mktime(&default_time);
+    struct timeval tv = {
+        .tv_sec = default_time_secs,
+        .tv_usec = 0
+    };
+    printf("Setting default time to %lld\n", default_time_secs);
+    settimeofday(&tv, NULL);
+
+    // Initialize the RTC GPIO
+    printf("Initializing RTC GPIO\n");
+    ret = rtc_gpio_init(GPIO_NUM_26);
+    if (ret != ESP_OK) {
+        printf("Error initializing RTC GPIO: %d\n", ret);
+        return;
+    }
+    ret = rtc_gpio_set_direction(GPIO_NUM_26, RTC_GPIO_MODE_INPUT_ONLY);
+    if (ret != ESP_OK) {
+        printf("Error setting RTC GPIO direction: %d\n", ret);
+        return;
+    }
+
+    xTaskCreate(&read_rtc, "read_rtc", 2048, NULL, 5, NULL);
 }
